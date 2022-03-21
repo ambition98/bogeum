8e6341c051db9351d39d09f2cc32f8ef40df888f
YJ_Lee <ambition65@naver.com>
2022-03-21 23:59:16 +0900

    🔨Refactor(entity): Bogeum, BogeumMember 와의 연관관계 매핑 설정

--- src/main/java/com/bogeum/web/entity/AccountEntity.java
+++ src/main/java/com/bogeum/web/entity/AccountEntity.java
@@ -2,45 +2,55 @@ package com.bogeum.web.entity;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
 @Entity
-@Table(name = "account")
+@Table(name = "ACCOUNT")
 public class AccountEntity implements Serializable {
 	
 	private static final long serialVerionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Column(name = "accountNo", nullable = false)
 	private int no;
 	
-	@Column(length = 64)
+	@Column(length = 50, nullable = false)
 	private String email;
 	
-	@Column(length = 256)
-	private String image_path;
+	@Column(length = 200, nullable = false)
+	private String imagePath;
 	
-	@Column(length = 1)
-	boolean is_verified;
+	@Column(length = 1, nullable = false)
+	private boolean isVerified;
 	
-	@Column(length = 256)
+	@Column(length = 250, nullable = true)
 	private String accessToken;
 	
-	@Column(length = 256)
+	@Column(length = 250, nullable = true)
 	private String refreshToken;
 	
+	@Column(nullable = false)
 	private Timestamp regdate;
+	
+	/* ---- 연관 관계 --- */
+	@OneToMany(mappedBy = "account")
+	private List<BogeumEntity> bogeum;
+	
+	@OneToMany(mappedBy = "account")
+	private List<BogeumMemberEntity> bogeum_member;
 }
