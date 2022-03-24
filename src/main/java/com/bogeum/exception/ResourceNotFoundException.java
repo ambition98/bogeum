d6ba4adaa5e87be556c5a680d15b3065f42a782e
YJ_Lee <ambition65@naver.com>
2022-03-25 00:30:35 +0900

    ✨Feat(exception): jpa find* 메서드 사용 시 해당 Id 엔티티가 존재하지 않을 시 던질 예외 추가

---
+++ src/main/java/com/bogeum/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.bogeum.exception;
+
+/**
+ * @author YJ_Lee
+ * 
+ * JPA에서 해당 entity를 찾지 못하였을 경우 던지는 예외
+ * Service 단에서 구현하는 것을 추천
+ */
+
+public class ResourceNotFoundException extends Exception {
+	
+	public ResourceNotFoundException() {}
+	
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
