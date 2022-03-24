fc60631a6d37e5d7611d11d9f1df8d798e256ace
YJ_Lee <ambition65@naver.com>
2022-03-25 00:29:07 +0900

    ✨Feat(util): 패스워드 검증 유틸 메서드 추가, 메서드 이름 변경

--- src/main/java/com/bogeum/util/CheckStringValidation.java
+++ src/main/java/com/bogeum/util/CheckStringValidation.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class CheckStringValidation {
 	
-	public boolean checkValidEmailPattern(String email) {
+	public static boolean checkEmail(String email) {
 		return Pattern.matches("/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i", email);
+	}
+	
+	public static boolean checkPassword(String password) {
+		return Pattern.matches("/^^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$/i", password);
 	}
 }
