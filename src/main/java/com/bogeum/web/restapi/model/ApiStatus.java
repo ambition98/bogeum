3147f2519cb643ae91d09347870b050064e2e88f
YJ_Lee <ambition65@naver.com>
2022-03-29 21:22:22 +0900

    ✨Feat(ApiStatus): Api 응답코드 추가

--- src/main/java/com/bogeum/web/restapi/model/ApiStatus.java
+++ src/main/java/com/bogeum/web/restapi/model/ApiStatus.java
@@ -21,9 +21,11 @@ public enum ApiStatus {
 	INVALID_STRING_PATTERN(-2, "Invalid string pattern"),
 	NOT_A_NUMBER(-3, "Not a number"),
 	RESOURCES_ARE_NOT_FOUND(-4, "Resources are not found"),
+	API_SERVER_ERROR(-5, "API server error"),
 	
 	// -199 ~ -100: User account 관련 오류 
-	DUPLICATED_EMAIL(-100, "Already used email");
+	DUPLICATED_EMAIL(-100, "Email is already used"),
+	INVALID_EMAIL_PATTERN(-101, "Invalid email pattern");
 	
 	private int code;
 	private String msg;
