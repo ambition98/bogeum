ee9e0f58d6f083472c2d99f505a8e0845c6b8895
YJ_Lee <ambition65@naver.com>
2022-03-29 21:21:24 +0900

    ✨Feat(util): HashingUtil 추가 및 유틸리티 관련은 빈 등록에서 제외 (static 메서드로 사용)

--- src/main/java/com/bogeum/util/CheckStringValidation.java
+++ src/main/java/com/bogeum/util/CheckStringValidation.java
@@ -2,16 +2,13 @@ package com.bogeum.util;
 
 import java.util.regex.Pattern;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class CheckStringValidation {
 	
 	public static boolean checkEmail(String email) {
-		return Pattern.matches("/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i", email);
+		return Pattern.matches("\\w+@\\w+\\.\\w+(\\.\\w+)?", email);
 	}
 	
 	public static boolean checkPassword(String password) {
-		return Pattern.matches("/^^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$/i", password);
+		return Pattern.matches("(?=.*[!@#$%&*?])(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z!@#$%&*?\\d]{8,}", password);
 	}
 }
